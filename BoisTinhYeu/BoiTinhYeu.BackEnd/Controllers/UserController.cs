046cd620762e19b2519034af735a08b22536e19f
NguyenThiNhu-Y <82461929+NguyenThiNhu-Y@users.noreply.github.com>
2021-12-18 17:39:00 +0700

    api

---
+++ BoisTinhYeu/BoiTinhYeu.BackEnd/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using BoiTinhYeu.Application.UserService;
+using BoiTinhYeu.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoiTinhYeu.BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IManagerUserService _managerUserService;
+        public UserController(IManagerUserService managerUserService)
+        {
+            _managerUserService = managerUserService;
+        }
+
+        [HttpGet("{username}")]
+        public IActionResult getByUsername(string username)
+        {
+            var user = _managerUserService.GetByUsername(username);
+            if(user == null)
+            {
+                return BadRequest("Cannot find user");
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult create(User user)
+        {
+            var newUser = _managerUserService.Create(user);
+            return Ok(newUser);
+        }
+    }
+}
